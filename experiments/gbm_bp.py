b710712e80d74dc2c815b1ba7f2f6a9f216f9d7b
workoapbl <liu.gwyneth@gmail.com>
2025-04-23 02:05:44 -0400

    updated BP with more params, random walk obs, obs testing code

--- experiments/gbm_bp.py
+++ experiments/gbm_bp.py
@@ -18,11 +18,11 @@ from observe import sample_observations, get_coordinate_distance
 import numpy as np
 import networkx as nx
 
+from random_walk_obs import random_walk_observations
 def create_observed_subgraph(num_coords, observations):
     """
     create subgraph containing all nodes and observed paths as edges
     """
-    
     subG = nx.Graph()
     
     for c in range(num_coords):
@@ -35,60 +35,50 @@ def create_observed_subgraph(num_coords, observations):
 
 if __name__ == "__main__":
     # Generate a graph with two clusters
-    # distributions = ['normal', 'normal']
-    # dist_params = [
-    #     {'loc':-0.5, 'scale':0.3, 'constrain_to_unit_sphere':False},
-    #     {'loc':0.5, 'scale':0.4, 'constrain_to_unit_sphere':False}
-    # ]
+    distributions = ['normal', 'normal', 'normal']
+    dist_params = [
+        {'loc':[-0.3, 0.1, 0.3], 'scale':0.2, 'constrain_to_unit_sphere':True},
+        {'loc':[0.1, 0.4, -0.2], 'scale':0.1, 'constrain_to_unit_sphere':True},
+        {'loc':[0.3, -0.1, 0.1], 'scale':0.2, 'constrain_to_unit_sphere':True},
+    ]
     
-    # G2, coords2, cluster_map2 = generate_latent_geometry_graph(
-    #     [100, 150], 
-    #     distributions=distributions,
-    #     dist_params=dist_params,
-    #     connectivity_threshold=0.8  # Higher threshold to create more edges
-    # )
-    num_nodes = 300
-    num_communities = 3
-    interior_prob = 0.7  # Increased internal connectivity
-    exterior_prob = 0.2  # Decreased external connectivity
-    G = get_sbm(num_nodes, num_communities, interior_prob, exterior_prob)
-    for node in G.nodes():
-        # assign random coordinates to each node depending on the community in 2D
-        community = G.nodes[node]['block']
-        if community == 0:
-            center = np.array([-0.4, 0.3])
-        elif community == 1:
-            center = np.array([-0.3, -0.4])
-        elif community == 2:
-            center = np.array([0.1, 0.2])
-        else:
-            center = np.array([0, 0.5])
-        G.nodes[node]['coords'] = np.random.normal(loc=center, scale=0.1, size=2)
-        
     def weight_func(coord1, coord2):
         distance = get_coordinate_distance(coord1, coord2)
-        return np.exp(-0.3 * distance)
+        return np.exp(-0.4 * distance)
     
-    observations = sample_observations(G, 20, weight_func=weight_func)    
+    def prob_weight_func(dist):
+        return max(np.exp(-0.6 * dist), 0.1)
+    
+    G2, coords2, cluster_map2 = generate_latent_geometry_graph(
+        [40, 20, 40], 
+        distributions=distributions,
+        dist_params=dist_params,
+        edge_prob_fn= prob_weight_func # Higher threshold to create more edges
+    )  
+    # observations, _ = sample_observations(G2, 25, weight_func=weight_func)
+    observations = random_walk_observations(G2, num_walkers=8, stopping_param=0.2, leaky=0.1)
     observed_nodes = set()
     for u, v in observations:
         observed_nodes.add(u)
         observed_nodes.add(v)
-    subG = create_observed_subgraph(num_nodes, observations)
-    subG = G
-    initialize_beliefs(subG, 2)
+    # print(observations)
     
+    subG = create_observed_subgraph(100, observations)
+    # subG = G2 
+    initialize_beliefs(subG, 3)
     belief_propagation(
         subG, 
-        num_communities, 
+        3, 
         max_iter=1000,
-        damping=0.2,
-        anneal_steps=300,    
-        balance_regularization=0.2,
-    )    
+        damping=0.4,
+        anneal_steps=100,    
+        balance_regularization=0.02,
+        min_steps=50,
+    )
     marginals, preds = get_marginals_and_preds(subG)
-    cluster_map = get_true_communities(G)
+    cluster_map = np.array(cluster_map2)
     sub_preds = np.array([preds[i] for i in range(len(preds)) if i in observed_nodes])
+    print(sub_preds)
     sub_cluster_map = np.array([cluster_map[i] for i in range(len(cluster_map)) if i in observed_nodes])
     print(detection_stats(preds, cluster_map))
     print(detection_stats(sub_preds, sub_cluster_map))
